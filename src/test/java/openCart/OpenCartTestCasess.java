97d949dcae025f52ea323a368bc6fac72b4de14a
mohammedrali <mohammedrali2007@gmail.com>
2022-11-15 03:32:13 -0500

    Add Test case01

---
+++ src/test/java/openCart/OpenCartTestCasess.java
@@ -0,0 +1,11 @@
+package openCart;
+
+import org.testng.annotations.Test;
+
+public class OpenCartTestCasess {
+@Test
+	public void loginTest() {
+		System.out.println("This is loging Test");
+	}
+	
+}
